2f7081b6ae33a75026303c740d91770e67f00856
Claire Besset <clairebesset@Claires-MacBook-Pro.local>
2025-01-18 15:44:22 -0500

    add OG image

--- pages/index.vue
+++ pages/index.vue
@@ -6,7 +6,7 @@ useSeoMeta({
   ogTitle: 'Instant, Authentic Social Media & Blog Content',
   description: 'Effortlessly create social posts and blogs that actually sound like you. From idea to authentic posts in seconds - get results across Twitter, LinkedIn, SEO and email. Try it free.',
   ogDescription: 'Effortlessly create social posts and blogs that actually sound like you. From idea to authentic posts in seconds - get results across Twitter, LinkedIn, SEO and email. Try it free.',
-  // ogImage: 'https://example.com/image.png',
+  ogImage: '/OG_image.png',
   // twitterCard: 'summary_large_image',
 })
 
